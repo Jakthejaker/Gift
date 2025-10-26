e097d7f77ebebec0cab52bf86aec036c02932f38
Jakthejaker <jainishpatil3@gmail.com>
2025-10-26 13:14:42 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,28 +1,27 @@
-from flask import Flask, render_template_string, send_from_directory
+from flask import Flask, render_template, send_from_directory
 import os
 
 # Get the absolute path of the directory where this script is located
-# This makes sure we always find files relative to server.py
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-app = Flask(__name__)
+# Tell Flask that our 'templates' (index.html) and
+# 'static' files (style.css) are in this SAME directory.
+app = Flask(__name__,
+            template_folder=APP_ROOT,
+            static_folder=APP_ROOT)
 
-# This route will serve the main index.html file.
 @app.route('/')
 def home():
-    # Create an absolute path to index.html
-    index_path = os.path.join(APP_ROOT, 'index.html')
-    with open(index_path, 'r') as f:
-        html_content = f.read()
-    return render_template_string(html_content)
+    # 'render_template' will now correctly look for 'index.html'
+    # in the 'template_folder' we defined above (which is APP_ROOT)
+    return render_template('index.html')
 
-# This route is needed to serve the CSS file
 @app.route('/style.css')
 def serve_css():
-    # Serve the style.css file from the same directory as this script
-    return send_from_directory(APP_ROOT, 'style.css')
+    # This will correctly serve 'style.css' from the
+    # 'static_folder' we defined above (which is APP_ROOT)
+    return send_from_directory(app.static_folder, 'style.css')
 
 if __name__ == '__main__':
-    # Render expects the app to bind to 0.0.0.0 and use the PORT env variable
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
