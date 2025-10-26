5dc36c12cb5b7400c28eff4d86543bb33a35a15c
Jakthejaker <jainishpatil3@gmail.com>
2025-10-26 12:57:44 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,28 @@
 from flask import Flask, render_template_string, send_from_directory
 import os
 
+# Get the absolute path of the directory where this script is located
+# This makes sure we always find files relative to server.py
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 app = Flask(__name__)
 
 # This route will serve the main index.html file.
 @app.route('/')
 def home():
-    with open(os.path.join('index.html'), 'r') as f:
+    # Create an absolute path to index.html
+    index_path = os.path.join(APP_ROOT, 'index.html')
+    with open(index_path, 'r') as f:
         html_content = f.read()
     return render_template_string(html_content)
 
 # This route is needed to serve the CSS file
 @app.route('/style.css')
 def serve_css():
-    return send_from_directory(os.getcwd(), 'style.css')
+    # Serve the style.css file from the same directory as this script
+    return send_from_directory(APP_ROOT, 'style.css')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    # Render expects the app to bind to 0.0.0.0 and use the PORT env variable
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
